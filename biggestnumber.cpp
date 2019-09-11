a1362f3b0abd5e230c8f76a2fd20016ced0e4713
Excalibur1224 <bafalla@mail.csuchico.edu>
2019-09-11 16:21:44 -0700

    Made the computer compare the two numbers

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -5,7 +5,7 @@ int main()
 {
     int first = 0;
     int second = 0;
-
+    int biggest = 0;
 
   std::cout<<"Please enter a whole number:\n";
   std::cin>>first;
@@ -13,11 +13,24 @@ int main()
   std::cout<<"Please enter another whole number:\n";
   std::cin>>second; 
 
-  
+  if (first > second)
+    {
+        std::cout<<"Of those two numbers, the biggest is: "<<first<<std::endl;
+  }
+  else if (second > first) 
+    {
+        std::cout<<"Of those two numbers, the biggest is: "<<second<<std::endl;
+  }
 
+  else if (first == second)
+    {
+        std::cout<<"Both integers are the same"<<std::endl;
+  }
 
-  std::cout<<"Of those two numbers, the biggest is: ";
-  
+  else
+    {
+        std::cout<<"?"<<std::endl;
+   }
 
   std::cout<<std::endl<<"Thank you for playing.\n";
   
