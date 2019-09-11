eef2f88483fd0e4c7eed7e984119c0ce92731644
Excalibur1224 <bafalla@mail.csuchico.edu>
2019-09-11 16:09:06 -0700

    Added user input capture

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -3,19 +3,21 @@
 
 int main()
 {
-  //declare variables here
+    int first = 0;
+    int second = 0;
+
 
   std::cout<<"Please enter a whole number:\n";
-  //get first input using std::cin>>
+  std::cin>>first;
 
   std::cout<<"Please enter another whole number:\n";
-  //get second input using std::cin>> again with a second variable
+  std::cin>>second; 
 
-  //determine if first or second is bigger and store that in a third variable
+  
 
 
   std::cout<<"Of those two numbers, the biggest is: ";
-  //print out the variable representing the biggest number
+  
 
   std::cout<<std::endl<<"Thank you for playing.\n";
   
